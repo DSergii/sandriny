c7e8b6f256384a496bd58b470312c34c2cc75798
DSergii <drozdsa@gmail.com>
2016-11-04 00:33:17 +0200

    add json

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+'use strict';
 $(document).ready(function() {
 	SandrinyApp.init();
 });
@@ -7,20 +8,67 @@ var SandrinyApp = {
 	init: function(){
 
 		this.showSocial();
-
+		this.showModal();
+		this.scrollLoad();
+		this.smoothShow();
 
 	},
-
+	/* show/hide social icons in model */
 	showSocial: function() {
-		var holder = $('.social-box');
+		let holder = $('.social-box');
 
 		holder.each(function() {
-			var box = $(this);
+			let box = $(this);
 
 			box.click(function(){
 				$(this).toggleClass('open');
 			});
 		})
+	},
+	/* show/hide modal */
+	showModal: function() {
+
+		let modalBtn = $('.show-modal'),
+			modal = null,
+			closeModal = null;
+
+		modalBtn.click(function() {
+			let data = $(this).data('id');
+			modal = $('#'+data).addClass('show');
+			closeModal = modal.find('.close');
+
+			closeModal.click(function(){
+				modal.removeClass('show');
+			})
+
+			return false;
+		});
+	},
+	/* load models when scrolling page */
+	scrollLoad: function() {
+		let spinner = $('.spinner');
+
+		$(window).scroll(function() {
+		    if($(window).scrollTop() == $(document).height() - $(window).height()) {
+		           //spinner.addClass('show');
+		           $.getJSON('stairs.json', function(data){
+		           		console.log(data);
+		           });
+		    }else {
+		    	//spinner.removeClass('show');
+		    }
+		});
+
+	},
+
+	smoothShow: function() {
+		let box = $('.box-item');
+
+		for (let i = 0; i < box.length; i++) {
+			setTimeout(function(){
+				$(box[i]).addClass('show');
+			}, 300*i);
+		}
 	}
 
 
